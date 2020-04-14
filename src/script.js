35f7fe7222dc2a759aa150c27fac17ac629fc8d3
Alexandra Burdina <sunh2oalex@gmail.com>
2020-04-14 15:26:09 +0300

    fix: fix variable names

--- src/script.js
+++ src/script.js
@@ -19,106 +19,97 @@ const CodesOfAllButtons = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 
   'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
 const CodesOfSpecialButtons = ['Backspace', 'Tab', 'Delete', 'CapsLock', 'Enter', 'ShiftLeft', 'ArrowUp', 'ShiftRight',
   'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
+let isCapsLockActive = 0;
 const arrayOfButtons = [];
-let CapsLockCounter = 0;
 const LayoutMouthKeyboatd = [];
 const Document = this.document;
+const colorForButtons = '#444444';
+const colorForSpecialButtons = '#222222';
+const changingElementNumber = [0, 13, 15, 28, 30, 41, 43, 54];
 
 class Button {
   constructor(nameOfBut) {
-    this.type = Document.createElement('div');
-    this.type.style.cssText = 'height: 40px; background: #444444; color: white; display: flex; justify-content: center; align-items: center;  border-radius: 10%; margin-left: 5px; margin-bottom: 5px;';
+    this.elem = Document.createElement('div');
+    this.elem.classList.add('button');
     switch (nameOfBut) {
       case 'BackSpace':
-        this.cssStyleForButton('100px', 'button different BackSpace');
+        this.setButtonStyle('100px', 'button different BackSpace');
         break;
       case 'Del':
-        this.cssStyleForButton('45px', 'button different Del');
+        this.setButtonStyle('45px', 'button different Del');
         break;
       case 'Tab':
-        this.cssStyleForButton('50px', 'button different Tab');
+        this.setButtonStyle('50px', 'button different Tab');
         break;
       case 'Caps Lock':
-        this.cssStyleForButton('100px', 'button different CapsLock');
+        this.setButtonStyle('100px', 'button different CapsLock');
         break;
       case 'Enter': case 'Shift ':
-        this.cssStyleForButton('85px', 'button different Enter');
+        this.setButtonStyle('85px', 'button different Enter');
         break;
       case 'Shift':
-        this.cssStyleForButton('100px', 'button different Shift');
+        this.setButtonStyle('100px', 'button different Shift');
         break;
       case 'Ctrl': case 'Ctrl ':
-        this.cssStyleForButton('40px', 'button different Ctrl');
+        this.setButtonStyle('40px', 'button different Ctrl');
         break;
       case 'Win':
-        this.cssStyleForButton('40px', 'button different Win');
+        this.setButtonStyle('40px', 'button different Win');
         break;
       case 'Alt':
-        this.cssStyleForButton('40px', 'button different Alt');
+        this.setButtonStyle('40px', 'button different Alt');
         break;
       case '←':
-        this.cssStyleForButton('40px', 'button different left');
+        this.setButtonStyle('40px', 'button different left');
         break;
       case '→':
-        this.cssStyleForButton('40px', 'button different right');
+        this.setButtonStyle('40px', 'button different right');
         break;
       case '↑':
-        this.cssStyleForButton('40px', 'button different top');
+        this.setButtonStyle('40px', 'button different top');
         break;
       case '↓':
-        this.cssStyleForButton('40px', 'button different down');
+        this.setButtonStyle('40px', 'button different down');
         break;
       case ' ':
-        this.cssStyleForButton('325px', 'button different Space');
-        this.type.style.borderRadius = '5px';
+        this.setButtonStyle('325px', 'button different Space');
+        this.elem.style.borderRadius = '5px';
         break;
       default:
-        this.type.style.width = '40px';
-        this.className('button simple');
+        this.elem.style.width = '40px';
+        this.elem.className = 'button simple';
         break;
     }
-    this.innerDiv(nameOfBut);
-    arrayOfButtons.push(this.type);
+    this.setDisplayValue(nameOfBut);
+    arrayOfButtons.push(this.elem);
   }
 
-  cssStyleForButton(width, cls) {
-    this.type.style.width = width;
-    this.type.style.backgroundColor = '#222222';
-    this.className(cls);
+  setButtonStyle(width, cls) {
+    this.elem.style.width = width;
+    this.elem.style.backgroundColor = colorForSpecialButtons;
+    this.elem.className = cls;
   }
 
-  innerDiv(value) {
-    this.type.innerText = value;
-  }
-
-  className(value) {
-    this.type.className = value;
-  }
-
-  appendButton(value) {
-    this.type.append(value.getType);
-  }
-
-  get getType() {
-    return this.type;
+  setDisplayValue(value) {
+    this.elem.innerText = value;
   }
 }
 
 /* Add containers */
 const container = Document.createElement('div');
 Document.body.append(container);
-container.style.cssText = 'width: 700px; margin: 0 auto;';
+container.classList.add('container');
 const title = Document.createElement('p');
 container.append(title);
 title.innerHTML = 'Виртуальная клавиатура';
-title.style.cssText = 'width: 700px; text-align: center; color: #F3B7B6; margin: 5px auto; font-size: 24px';
+title.classList.add('title');
 const textarea = Document.createElement('textarea');
 container.append(textarea);
 textarea.value = '';
-textarea.style.cssText = 'width: 690px; height: 300px; margin-bottom: 7px';
+textarea.classList.add('textarea');
 const keyboard = Document.createElement('div');
 textarea.after(keyboard);
-keyboard.style.cssText = 'background-color: #888888; padding-right:5px; padding-top:5px; width:691px';
+keyboard.classList.add('keyboard');
 
 /* Add rows */
 const rows = [Document.createElement('div'), Document.createElement('div'), Document.createElement('div'), Document.createElement('div'), Document.createElement('div')];
@@ -130,20 +121,20 @@ for (let i = 0; i < rows.length; i += 1) {
 const text = Document.createElement('p');
 container.append(text);
 text.innerHTML = 'Виртуальная клавиатура создана в операционной системе Windows \n Для переключения языка: комбинация ctrl + alt';
-text.style.cssText = 'width: 600px; text-align: center; color: #CD72D1; margin: 5px auto; font-size: 20px';
+text.classList.add('text');
 
 /* Add buttons to the screen */
-const ruOrEng = this.localStorage.getItem('languige');
+const currentLanguage = this.localStorage.getItem('languige');
 let activeLanguage;
-if (ruOrEng === 'eng') activeLanguage = EnglishKeyboard;
+if (currentLanguage === 'eng') activeLanguage = EnglishKeyboard;
 else activeLanguage = RussianKeyboard;
 for (let i = 0; i < activeLanguage.length; i += 1) {
   const temp = new Button(activeLanguage[i]);
-  if (i < 14) rows[0].append(temp.getType);
-  else if (i < 29) rows[1].append(temp.getType);
-  else if (i < 42) rows[2].append(temp.getType);
-  else if (i < 55) rows[3].append(temp.getType);
-  else rows[4].append(temp.getType);
+  if (i < 14) rows[0].append(temp.elem);
+  else if (i < 29) rows[1].append(temp.elem);
+  else if (i < 42) rows[2].append(temp.elem);
+  else if (i < 55) rows[3].append(temp.elem);
+  else rows[4].append(temp.elem);
 }
 
 /* Mouse hover */
@@ -153,16 +144,16 @@ Document.addEventListener('mouseover', (event) => {
   Document.addEventListener('mouseout', (e) => {
     if (e.target.classList.contains('button')) {
       if (backgroundOfButton === 'rgb(68, 68, 68)') {
-        e.target.style.backgroundColor = '#444444';
-      } else e.target.style.backgroundColor = '#222222';
+        e.target.style.backgroundColor = colorForButtons;
+      } else e.target.style.backgroundColor = colorForSpecialButtons;
       if (e.target.classList.contains('Space')) e.target.style.borderRadius = '5px';
       else e.target.style.borderRadius = '10%';
     }
     if (e.target.classList.contains('CapsLock')) {
-      if (CapsLockCounter === 1) {
+      if (isCapsLockActive === 1) {
         arrayOfButtons[29].style.backgroundColor = '#F3B7B6';
       } else {
-        e.target.style.backgroundColor = '#222222';
+        e.target.style.backgroundColor = colorForSpecialButtons;
       }
     }
   });
@@ -170,37 +161,42 @@ Document.addEventListener('mouseover', (event) => {
 
 /* functions for changing the value of buttons */
 
-function toUppercaseLowercase(val1, val2, upcase) {
-  for (let i = val1; i < val2; i += 1) {
-    if (upcase === 'toUpperCase') arrayOfButtons[i].innerText = arrayOfButtons[i].innerText.toUpperCase();
-    else arrayOfButtons[i].innerText = arrayOfButtons[i].innerText.toLowerCase();
+function isLanguigeRussian() {
+  if (arrayOfButtons[0].innerText === 'ё' || arrayOfButtons[0].innerText === 'Ё') return true;
+  return false;
+}
+function toUppercaseLowercase(from, to, caseType) {
+  for (let i = from; i < to; i += 1) {
+    if (caseType === 'toUpperCase') {
+      arrayOfButtons[i].innerText = arrayOfButtons[i].innerText.toUpperCase();
+    } else arrayOfButtons[i].innerText = arrayOfButtons[i].innerText.toLowerCase();
   }
 }
-function capsLockFunction(upLowCase) {
-  let temp;
-  if (upLowCase === 'Upp') temp = 'toUpperCase';
-  else temp = 'roLowerCase';
-  if (arrayOfButtons[0].innerText === 'ё' || arrayOfButtons[0].innerText === 'Ё') {
-    toUppercaseLowercase(0, 1, temp);
-    toUppercaseLowercase(15, 27, temp);
-    toUppercaseLowercase(30, 41, temp);
-    toUppercaseLowercase(43, 52, temp);
+function capsLockHandler(buttonRegistor) {
+  let caseType;
+  if (buttonRegistor === 'Upp') caseType = 'toUpperCase';
+  else caseType = 'toLowerCase';
+  if (isLanguigeRussian()) {
+    toUppercaseLowercase(0, 1, caseType);
+    toUppercaseLowercase(15, 27, caseType);
+    toUppercaseLowercase(30, 41, caseType);
+    toUppercaseLowercase(43, 52, caseType);
   } else {
-    toUppercaseLowercase(15, 25, temp);
-    toUppercaseLowercase(30, 39, temp);
-    toUppercaseLowercase(43, 50, temp);
+    toUppercaseLowercase(15, 25, caseType);
+    toUppercaseLowercase(30, 39, caseType);
+    toUppercaseLowercase(43, 50, caseType);
   }
 }
-function changeColorOfButtons(val1, val2, color) {
-  for (let i = val1; i < val2; i += 1) {
+function changeColorOfButtons(from, to, keyType) {
+  for (let i = from; i < to; i += 1) {
     if (this.event.code === CodesOfAllButtons[i]) {
-      if (color === 'down') {
+      if (keyType === 'down') {
         arrayOfButtons[i].style.backgroundColor = '#E49CB2';
         arrayOfButtons[i].style.borderRadius = '50%';
         textarea.value += arrayOfButtons[i].innerText;
         return true;
       }
-      arrayOfButtons[i].style.backgroundColor = '#444444';
+      arrayOfButtons[i].style.backgroundColor = colorForButtons;
       arrayOfButtons[i].style.borderRadius = '5px';
       return true;
     }
@@ -208,10 +204,10 @@ function changeColorOfButtons(val1, val2, color) {
   return false;
 }
 
-function uppOnShift(val1, val2, uplowcase, count) {
+function uppOnShift(from, to, caseType, count) {
   let temp = count;
-  for (let i = val1; i < val2; i += 1) {
-    if (uplowcase === 'down') arrayOfButtons[i].innerText = EnglishKeyboardShift[temp];
+  for (let i = from; i < to; i += 1) {
+    if (caseType === 'down') arrayOfButtons[i].innerText = EnglishKeyboardShift[temp];
     else arrayOfButtons[i].innerText = EnglishKeyboard[i];
     temp += 1;
   }
@@ -219,20 +215,20 @@ function uppOnShift(val1, val2, uplowcase, count) {
 }
 
 
-function cyclyForChangingColorOfButtons(color) {
-  uppOnShift(50, 53, color,
-    uppOnShift(39, 41, color,
-      uppOnShift(25, 28, color,
-        uppOnShift(0, 13, color, 0))));
+function cyclyForChangingColorOfButtons(caseType) {
+  uppOnShift(50, 53, caseType,
+    uppOnShift(39, 41, caseType,
+      uppOnShift(25, 28, caseType,
+        uppOnShift(0, 13, caseType, 0))));
 }
 
-function handlerOfShift(color) {
-  if (arrayOfButtons[0].innerText === 'ё' || arrayOfButtons[0].innerText === 'Ё') {
+function handlerOfShift(caseType) {
+  if (isLanguigeRussian()) {
     for (let i = 0; i < 13; i += 1) {
-      if (color === 'down') arrayOfButtons[i].innerText = RussianKeyboardShift[i];
+      if (caseType === 'down') arrayOfButtons[i].innerText = RussianKeyboardShift[i];
       else arrayOfButtons[i].innerText = RussianKeyboard[i];
     }
-    if (color === 'down') {
+    if (caseType === 'down') {
       arrayOfButtons[27].innerText = RussianKeyboardShift.slice(13, 14);
       arrayOfButtons[52].innerText = RussianKeyboardShift.slice(14, 15);
     } else {
@@ -240,31 +236,31 @@ function handlerOfShift(color) {
       arrayOfButtons[52].innerText = RussianKeyboard.slice(52, 53);
     }
   } else {
-    cyclyForChangingColorOfButtons(color);
+    cyclyForChangingColorOfButtons(caseType);
   }
-  if (color === 'down') capsLockFunction('Upp');
-  else capsLockFunction('Low');
+  if (caseType === 'down') capsLockHandler('Upp');
+  else capsLockHandler('Low');
 }
 
 function handlerOfCapsLock() {
-  if (CapsLockCounter === 0) {
+  if (isCapsLockActive === 0) {
     arrayOfButtons[29].style.backgroundColor = '#F3B7B6';
-    capsLockFunction('Upp');
-    CapsLockCounter = 1;
+    capsLockHandler('Upp');
+    isCapsLockActive = 1;
   } else {
-    arrayOfButtons[29].style.backgroundColor = '#222222';
+    arrayOfButtons[29].style.backgroundColor = colorForSpecialButtons;
     arrayOfButtons[29].style.transition = '1s';
     arrayOfButtons[29].style.borderRadius = '5px';
-    capsLockFunction('Low');
-    CapsLockCounter = 0;
+    capsLockHandler('Low');
+    isCapsLockActive = 0;
   }
 }
 
-function changeColorOfSpecialButtons(color) {
+function changeColorOfSpecialButtons(caseType) {
   const temp = CodesOfAllButtons.indexOf(this.event.code, 0);
-  const buttonInSpecialArray = CodesOfSpecialButtons.includes(this.event.code, 0);
-  if (temp !== -1 && buttonInSpecialArray === true) {
-    if (color === 'down') {
+  const isSpecialButton = CodesOfSpecialButtons.includes(this.event.code);
+  if (temp !== -1 && isSpecialButton) {
+    if (caseType === 'down') {
       arrayOfButtons[temp].style.backgroundColor = '#E49CB2';
       arrayOfButtons[temp].style.borderRadius = '40%';
       switch (temp) {
@@ -304,22 +300,22 @@ function changeColorOfSpecialButtons(color) {
     } else {
       if (temp === 55 || temp === 63 || temp === 57 || temp === 59) LayoutMouthKeyboatd.pop();
       if (temp !== 29) {
-        arrayOfButtons[temp].style.backgroundColor = '#222222';
+        arrayOfButtons[temp].style.backgroundColor = colorForSpecialButtons;
         arrayOfButtons[temp].style.borderRadius = '5px';
       }
     }
     if (temp === 54 || temp === 42) { /* Shift */
       textarea.value += '';
-      handlerOfShift(color);
+      handlerOfShift(caseType);
     }
   }
 }
 
 
 /* Chenge layout */
-function layoutSwitching() {
-  if (LayoutMouthKeyboatd.includes('Alt', 0) && LayoutMouthKeyboatd.includes('Ctrl', 0)) {
-    if (arrayOfButtons[0].innerText === 'ё' || arrayOfButtons[0].innerText === 'Ё') {
+function switchLayout() {
+  if (LayoutMouthKeyboatd.includes('Alt', 0) && LayoutMouthKeyboatd.includes('Ctrl')) {
+    if (isLanguigeRussian()) {
       for (let i = 0; i < EnglishKeyboard.length; i += 1) {
         arrayOfButtons[i].innerText = EnglishKeyboard[i];
       }
@@ -332,28 +328,27 @@ function layoutSwitching() {
     }
   }
 }
-/* Изменение значения кнопок */
+/* Chenging the value of buttons */
 
 Document.addEventListener('keydown', (event) => {
-  /* Иземенение цвета и вывод в input */
-  if (!CodesOfSpecialButtons.includes(event.code, 0)) {
-    if (!changeColorOfButtons(0, 13, 'down')) {
-      if (!changeColorOfButtons(15, 28, 'down')) {
-        if (!changeColorOfButtons(30, 41, 'down')) {
-          changeColorOfButtons(43, 53, 'down');
+  /* changing fo color and input */
+  if (!CodesOfSpecialButtons.includes(event.code)) {
+    if (!changeColorOfButtons(changingElementNumber[0], changingElementNumber[1], 'down')) {
+      if (!changeColorOfButtons(changingElementNumber[2], changingElementNumber[3], 'down')) {
+        if (!changeColorOfButtons(changingElementNumber[4], changingElementNumber[5], 'down')) {
+          changeColorOfButtons(changingElementNumber[6], changingElementNumber[7], 'down');
         }
       }
     }
   }
   changeColorOfSpecialButtons('down');
-  /* Переключение раскладки */
-  layoutSwitching();
+  switchLayout();
 });
 Document.addEventListener('keyup', () => {
-  if (!changeColorOfButtons(0, 13, 'up')) {
-    if (!changeColorOfButtons(15, 28, 'up')) {
-      if (!changeColorOfButtons(30, 41, 'up')) {
-        changeColorOfButtons(43, 54, 'up');
+  if (!changeColorOfButtons(changingElementNumber[0], changingElementNumber[1], 'up')) {
+    if (!changeColorOfButtons(changingElementNumber[2], changingElementNumber[3], 'up')) {
+      if (!changeColorOfButtons(changingElementNumber[4], changingElementNumber[5], 'up')) {
+        changeColorOfButtons(changingElementNumber[6], changingElementNumber[7], 'up');
       }
     }
   }
@@ -406,7 +401,7 @@ container.addEventListener('mousedown', (event) => {
       handlerOfShift('down');
     }
   }
-  layoutSwitching();
+  switchLayout();
 });
 Document.addEventListener('mouseup', (event) => {
   this.event.target.style.borderRadius = '5px';
